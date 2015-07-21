22ffbe0bb6f4229e6e68dfd05f1fb4ec004d1dde
ashutosh.yadav <ashutosh.yadav@shephertz.com>
2015-07-21 18:23:52 +0530

    changing env var

--- src/test/java/selenium/JunitSeleniumTest.java
+++ src/test/java/selenium/JunitSeleniumTest.java
@@ -19,8 +19,8 @@ public class JunitSeleniumTest {
     @Test
     public void myTest() throws Exception {
     	String sysEnvStr = System.getenv("JAVA_HOME");
-    	String sysEnvStr1 = System.getenv("JMeter");
-    	System.out.println(System.getEnv()+"Selenium Test Successfull"+sysEnvStr+"heheheh"+sysEnvStr1);
+    	String sysEnvStr1 = System.getenv("PATH");
+    	System.out.println("Selenium Test Successfull"+sysEnvStr+"heheheh"+sysEnvStr1);
         WebDriver driver = new RemoteWebDriver(
                                 new URL("http://92.246.242.230/wd/hub/"), 
                                 DesiredCapabilities.firefox());
